10b683c1c4722a497217d52d6d8293e57f541384
Abhishek Verma <nonuabi@gmail.com>
2026-02-26 15:42:33 +0530

    Enable vector extension in the database and update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_26_095154) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_26_100058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "vector"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
